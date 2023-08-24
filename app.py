52a576ad0fa9ac518e4e504801d31686ff3e4b1f
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-08-24 18:01:17 +0300

    u

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import yfinance as yf
 import streamlit as st
+from datetime import datetime
 from module import Fund, Account  # Assuming we saved our Fund and Account classes in 'fund_module.py'
 
 
@@ -9,31 +10,31 @@ import auth
 conn = auth.Connect()
 db = conn.get_collection('fund')
 
-st.write(db.document("fund").get().to_dict()['value'])
-
 docs = conn.get_collection('fund').stream()
 # items = list(map(lambda x: {'ticker': x.id, **x.to_dict()}, docs))
 items = [x.id for x in docs]
-st.write(items)
+latest_date = max(datetime.strptime(date, '%Y-%m-%d') for date in items)
+
+st.write(items, latest_date)
 
 
-fund_value = db.document("fund").get().to_dict()['value']
-fund_shares = db.document("fund").get().to_dict()['shares']
+# fund_value = db.document("fund").get().to_dict()['value']
+# fund_shares = db.document("fund").get().to_dict()['shares']
 
-st.metric('NAV', fund_value)
-st.metric('Shares', fund_shares)
-st.metric('Price', fund_value / fund_shares)
+# st.metric('NAV', fund_value)
+# st.metric('Shares', fund_shares)
+# st.metric('Price', fund_value / fund_shares)
 
-fund = Fund()
-saleh = Account("saleh", fund)
-ruyuf = Account("ruyuf", fund)
+# fund = Fund()
+# saleh = Account("saleh", fund)
+# ruyuf = Account("ruyuf", fund)
 
-account = st.radio('account', ['saleh', 'ruyuf'])
-amount = st.number_input('amount', min_value=0)
-submitted = st.button("Submit")
+# account = st.radio('account', ['saleh', 'ruyuf'])
+# amount = st.number_input('amount', min_value=0)
+# submitted = st.button("Submit")
 
-if submitted:
-    saleh.contribute(amount)
+# if submitted:
+#     saleh.contribute(amount)
 
 
 
