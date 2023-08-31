b2c5ee8099d6a3d9344c44c859cd8f6e569b15da
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-08-31 12:27:02 +0300

    u

--- pages/Transactions.py
+++ pages/Transactions.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.title('Transactions Page')
+
+tickers = st.multiselect('Choose ticker', ['VTI', 'AVUV', 'VXUS'])
+
+for i in tickers:
+    st.write(i, 'shares', 'cost', 'fee') 
