41aed9ed9621ce883ff630d004319ff52234b2cd
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-08-31 12:22:07 +0300

    u

--- pages/Portfolio.py
+++ pages/Portfolio.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 st.title('Portfolio Page')
 
-df = pd.DataFrame()
+df = pd.DataFrame(columns=['ticker', 'target'])
 
-st.data_editor(df)
+st.data_editor(df, num_rows='dynamic')
