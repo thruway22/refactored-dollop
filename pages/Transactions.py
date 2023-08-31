edc18dcb412612b92540bae2b4edb24ae50aa60d
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-08-31 12:27:54 +0300

    u

--- pages/Transactions.py
+++ pages/Transactions.py
@@ -4,5 +4,7 @@ st.title('Transactions Page')
 
 tickers = st.multiselect('Choose ticker', ['VTI', 'AVUV', 'VXUS'])
 
+st.write(tickers)
+
 for i in tickers:
     st.write(i, 'shares', 'cost', 'fee') 
